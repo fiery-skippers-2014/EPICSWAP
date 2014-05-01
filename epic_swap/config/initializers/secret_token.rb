d8646a569a068b2805a466a4cf1199e2e4e7d57e
Tyler Stauss <apprentice@dbc26.local>
2014-05-01 12:17:09 -0700

    initial commit

---
+++ epic_swap/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EpicSwap::Application.config.secret_token = '00e10f75515b402c70863fe720eadb2c6c029add60eafbd9dfd671fca88ea61daf1770396a4ac2373a1e7665abd723174297e77175a078c8337d999f06db9eb6'
