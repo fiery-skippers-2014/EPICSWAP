b7912e30253cba61fcb21f9d4a11150786d01b1b
Tyler Stauss <apprentice@dbc26.local>
2014-05-02 22:09:58 -0700

    A logged in user cannot add skills/interests on another users page, have accompanying test   TODO refactor html code.

--- spec/features/users/user_profile_spec.rb
+++ spec/features/users/user_profile_spec.rb
@@ -43,16 +43,27 @@ feature 'add skills on the user profile' do
 end
 
 feature 'add interests on the user profile' do
+  let!(:user) { FactoryGirl.create :user }
+  let!(:second_user) { FactoryGirl.create :user }
   before(:each) do
     ApplicationController.any_instance.stub(:current_user) { user }
   end
-  let!(:user) { FactoryGirl.create :user }
   scenario 'a user can add interests they can teach', js: true  do
     visit user_path(user)
     fill_in 'Interest', :with => 'white water rafting'
     click_on 'Create Interest'
     expect(page).to have_content 'My Interests'
     expect(page).to have_content 'white water rafting'
+  end
+
+  scenario 'a user who is not the current user cannot add skills' do
+    visit user_path(second_user)
+    expect(page).to_not have_content('Create Skill')
+  end
+
+  scenario 'a user who is not the current user cannot add interest' do
+    visit user_path(second_user)
+    expect(page).to_not have_content('Create Interest')
   end
 end
 
