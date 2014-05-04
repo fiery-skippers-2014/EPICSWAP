6be86bc39c503e917066bcc686f97331af6a287e
Tyler Stauss <tyler.e.stauss@gmail.com>
2014-05-03 22:23:02 -0700

    uniquness for skills and interests and tested

--- spec/models/interest/interest_spec.rb
+++ spec/models/interest/interest_spec.rb
@@ -8,5 +8,6 @@ describe Interest do
 
   context 'validations' do
     it {should validate_presence_of(:name) }
+    it {should validate_uniqueness_of(:name)}
   end
 end
--- spec/models/skills/skills_spec.rb
+++ spec/models/skills/skills_spec.rb
@@ -9,5 +9,6 @@ describe Skill do
 
   context 'validations' do
     it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
   end
 end
