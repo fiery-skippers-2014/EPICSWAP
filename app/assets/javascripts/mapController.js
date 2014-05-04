d818f22130678f5df2e7c5f09f7e3da0f0a294f2
Nathan Sass <nsass711@gmail.com>
2014-05-03 20:27:28 -0700

    Able to use JSON to add markers to map. Able attach windows that open with content. It should work with out existing mustache templates but I have not checked that yet. Overall I need to re-write all of the JS In order to accomadate the new workflow

--- app/assets/javascripts/mapController.js
+++ app/assets/javascripts/mapController.js
@@ -21,26 +21,69 @@ MapController.prototype = {
   _createMarkerForUser: function(userData){
     var category        = userData['category']
     var currentMarker   = this._buildMarker(userData["user"].latitude, userData["user"].longitude, category);
-    var html            = this._buildMustacheTemplate(userData);
-    currentMarker.bindPopup(html).openPopup();
+    currentMarker.addTo(this.COOL_MAP).bindPopup("<p>hi</p>").openPopup();
+    // var html            = this._buildMustacheTemplate(userData);
+    // currentMarker.bindPopup(html).openPopup();
   },
 
   _buildMarker: function(latitude, longitude, category){
-    var myIcon = L.icon({
-      iconUrl: '/assets/sample_icon.png',
-      // iconRetinaUrl: 'my-icon@2x.png',
-      iconSize: [38, 50],
-      iconAnchor: [22, 94],
-      popupAnchor: [-3, -76],
-      // shadowUrl: 'sample_icon',
-      // shadowRetinaUrl: 'my-icon-shadow@2x.png',
-      shadowSize: [68, 95],
-      shadowAnchor: [22, 94],
-      className: "coolMarkers"
-    });
+    var geoJsonData = {
+    "type": "FeatureCollection",
+    "features": [{ "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [longitude, latitude]
+        },
+        "properties": {
+            "title": "love", //$("[title=love]").hide()
+            "description": "A description",
+            "marker-size": "medium",
+            "marker-symbol": "heart",
+            "marker-color": "2963ba",
+            "stroke": "#555555",
+            "stroke-opacity": 1.0,
+            "stroke-width": 2,
+            "fill": "#555555",
+            "fill-opacity": 0.5,
+            "className": "awesome"
+        }
+    }]
+};
 
-    return L.marker([latitude, longitude],{icon: myIcon}).addTo(this.COOL_MAP); //location to add icon parameters, possible default icon
+    // Here we use the `L.geoJson` layer type from Leaflet, that lets us use
+    // Polygons, Points, and all other GeoJSON types - but we specify that it
+    // should also pull styles for polygons with the `style` option
+    // and use the custom `L.mapbox.marker.style` function
+    // to make fancy markers with the `pointToLayer` option.
+    // var geoJson = L.geoJson(geoJsonData, {
+    //     pointToLayer: L.mapbox.marker.style,
+    //     style: function(feature) { return feature.properties; }
+    // }).addTo(this.COOL_MAP)
+
+  var geoJson = L.geoJson(geoJsonData, {
+        pointToLayer: L.mapbox.marker.style,
+        style: function(feature) { return feature.properties; }
+    });
+  return geoJson
+
   },
+
+  // _buildMarker: function(latitude, longitude, category){
+  //   var myIcon = L.icon({
+  //     iconUrl: '/assets/sample_icon.png',
+  //     // iconRetinaUrl: 'my-icon@2x.png',
+  //     iconSize: [38, 50],
+  //     iconAnchor: [22, 94],
+  //     popupAnchor: [-3, -76],
+  //     // shadowUrl: 'sample_icon',
+  //     // shadowRetinaUrl: 'my-icon-shadow@2x.png',
+  //     shadowSize: [68, 95],
+  //     shadowAnchor: [22, 94],
+  //     className: "coolMarkers"
+  //   });
+
+  //   return L.marker([latitude, longitude],{icon: myIcon}).addTo(this.COOL_MAP); //location to add icon parameters, possible default icon
+  // },
 
   _buildMustacheTemplate: function(userData){
     var rawTemplate     = $("#userMarkerContentTemplate").html();
