44e71df991d110dd63a7ed6741ea14578b4170b0
Lyndsey Williams <lywilliams@gmail.com>
2014-05-03 17:15:02 -0700

    in the middle of learning about markers

--- app/assets/javascripts/mapController.js
+++ app/assets/javascripts/mapController.js
@@ -20,14 +20,44 @@ MapController.prototype = {
 
   _createMarkerForUser: function(userData){
     var category        = userData['category']
-    var currentMarker   = this._addLocation(userData["user"].latitude, userData["user"].longitude);
+    var currentMarker   = this._buildMarker(userData["user"].latitude, userData["user"].longitude, category);
     var html            = this._buildMustacheTemplate(userData);
     currentMarker.bindPopup(html).openPopup();
   },
 
-  _addLocation: function(latitude, longitude){
-    return L.marker([latitude, longitude]).addTo(this.COOL_MAP);
+  _buildMarker: function(latitude, longitude, category){
+    var iconForCategory = _iconByCategory(category)
+    return L.marker([latitude, longitude], { icon: iconForCategory }).addTo(this.COOL_MAP); //location to add icon parameters, possible default icon
   },
+
+  _iconByCategory: function(category){
+    if (category == "art"){
+      var artIcon = new MapMarker({iconUrl: })
+
+    }
+    else if (category == "cooking"){
+
+    }else if(category == "sports"){
+
+    }else{
+      // default here
+    }
+  },
+
+
+  _iconBaseClass: function(){
+    var CategoryIcon = L.Icon.extend({
+      options: {
+          shadowUrl: 'leaf-shadow.png',
+          iconSize:     [38, 95],
+          shadowSize:   [50, 64],
+          iconAnchor:   [22, 94],
+          shadowAnchor: [4, 62],
+          popupAnchor:  [-3, -76]
+      }
+    });
+  },
+
 
   _buildMustacheTemplate: function(userData){
     var rawTemplate     = $("#userMarkerContentTemplate").html();
