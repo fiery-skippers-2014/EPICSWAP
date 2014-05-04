a90c7bc0c5f6cf9fe322ec7e29ae7a7d35dec80d
Tyler Stauss <apprentice@dbc05.local>
2014-05-04 14:27:35 -0700

    removed p statement

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -20,7 +20,6 @@ class InterestsController < ApplicationController
   def show
     @interest = Interest.find(params[:id])
     @sorted_users = @interest.sort_users_by_distance(current_user)
-    p @sorted_users
   end
 
   def destroy
