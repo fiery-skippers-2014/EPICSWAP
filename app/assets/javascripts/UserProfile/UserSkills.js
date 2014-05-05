9a2ae08ca037d010d9ba6f6e49295ad661feddc8
Nathan Sass <nsass711@gmail.com>
2014-05-04 17:36:30 -0700

    accounts for deletes in the autocomplete of userskills, TODO - make it so a user can add a skill that already exists and it adopts the existing category

--- app/assets/javascripts/UserProfile/UserSkills.js
+++ app/assets/javascripts/UserProfile/UserSkills.js
@@ -1,5 +1,4 @@
 var UserSkills = {
-  query: '',
   init: function(){
     $('form.new_skill').on('ajax:success', this.appendSkill.bind(this) )
     $('form.new_skill').on('ajax:error', this.appendError )
@@ -9,41 +8,47 @@ var UserSkills = {
     $('div.skill_dropdown').on('click', 'li a', this.insertAutoComplete)
   },
   removeSkill: function(e,data){
-    var id = $(e.target).data('id')
-    $('li#skill_'+ id).remove()
+    var id = $(e.target).data('id');
+    $('li#skill_'+ id).remove();
   },
+
   appendSkill: function(e, data){
     console.log(data)
     $('ul.my-skills').append(data);
     $('form.new_skill').each(function(){
       this.reset();
     });
-    $('div.skill_dropdown').html('')
-    this.query = ''
+    $('div.skill_dropdown').html('');
   },
+
   appendError: function(e, data, f, g){
     $('ul.skill-form').prepend(data)
   },
+
   showError: function(e, data){
+    console.log("error in UserSkills.showError")
   },
+
   autoComplete: function(e){
-    this.query = this.query + String.fromCharCode(e.which);
-    var searchLetters = this.query
+    var searchLetters   = $("#skill_name").val();
     var checkDBforSkill = new AjaxClient("get", "/skills/autocomplete")
-    checkDBforSkill.request(searchLetters).done(this.onDataCompleteSuccess.bind(this))
-                             .fail(this.onDataCompleteFail.bind(this));
+    checkDBforSkill.request(searchLetters)
+                   .done(this.onDataCompleteSuccess.bind(this))
+                   .fail(this.onDataCompleteFail.bind(this));
   },
+
   onDataCompleteSuccess: function(data){
     $('div.skill_dropdown').html(data)
   },
+
   onDataCompleteFail: function(data){
-    console.log("we messed up maaaan")
+    console.log("Error in onDataCompleteFail")
   },
+
 
   insertAutoComplete: function(e){
     e.preventDefault();
     var skill = $(this).html()
     $('input#skill_name').val(skill)
-    // $('form.new_skill').trigger('ajax:success')
   }
 }
