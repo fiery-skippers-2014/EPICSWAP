310168fe680ea8ab639e1973e31d635b31139857
Nathan Sass <nsass711@gmail.com>
2014-05-04 19:03:07 -0700

    removing p statements

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -4,17 +4,13 @@ class SkillsController < ApplicationController
     @user = User.find(params[:user_id])
     skill_name = params[:skill][:name]
     skill_name.downcase! if skill_name != nil
-    p Skill.count
     @skill = Skill.find_or_create_by_name(skill_name)
-    p Skill.count
-
 
     if @skill.category_id == nil && @skill.name != nil
       @skill.update_attribute("category_id", params[:skill][:category_id])
     end
 
     if @skill.save
-      p Skill.count
       if Skill.relationship_exists(@user, @skill)
         @errors = "You already have #{@skill.name} in your skill list"
         render partial: 'shared/errors', locals: {errors: @errors}
@@ -24,7 +20,6 @@ class SkillsController < ApplicationController
       end
 
     else
-      p Skill.count
       @errors = 'Skill cannot be blank'
       render partial: 'shared/errors', locals: {errors: @errors}
     end
