533187baba64b3a954f3be128d4182660662eaf3
Ravi S. Rāmphal <rramphal@gmail.com>
2014-05-05 15:30:58 -0700

    updates

--- app/assets/javascripts/DropDownMenu.js
+++ app/assets/javascripts/DropDownMenu.js
@@ -1,9 +1,18 @@
 var DropDownMenu = {
   init: function(){
-    $('span#menu_dropdown').on('click', this.toggleMenu)
-
+    $('span#menu_dropdown').on('click', this.toggleMenu);
+    $('ul#dropdown_menu').slideUp();
   },
+
   toggleMenu: function(){
     $('ul#dropdown_menu').slideToggle();
+  },
+
+  menuUp: function() {
+    $('ul#dropdown_menu').slideUp();
+  },
+
+  menuDown: function() {
+    $('ul#dropdown_menu').slideDown();
   }
-}
+};
