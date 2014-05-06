9e0faff44edf86275b18be7171053c44f52f048a
Apprentice <apprentice@DBC-Infinity-2.local>
2014-05-05 17:26:10 -0700

    cursor for the filter element on map page

--- app/assets/javascripts/MapFilter.js
+++ app/assets/javascripts/MapFilter.js
@@ -9,7 +9,9 @@ var MapFilter = {
     $('div#map_filter_menu').slideToggle();
   },
   filterCategories: function(e){
+    e.preventDefault()
     var cat_title = $(e.target).data('title')
+    $('[title=' + cat_title + ']').toggle()
 
   }
 };
