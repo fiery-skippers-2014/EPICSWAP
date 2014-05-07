fd7a1b804b64955be7c7189bcdac8c0bde0fbfd8
Lyndsey Williams <lywilliams@gmail.com>
2014-05-07 14:09:47 -0700

    tests for about page

--- app/assets/javascripts/MapFilter.js
+++ app/assets/javascripts/MapFilter.js
@@ -1,20 +1,19 @@
 var MapFilter = {
   init: function() {
-    $('div#map_filter_menu').hide();
-    $('div#filter').on('click', this.toggleFilter);
+    $('#map_filter_menu').hide();
+    $('#filter').on('click', this.toggleFilter);
     $('.filter_link').on('click', this.filterCategories)
   },
 
   toggleFilter: function() {
-    $('div#map_filter_menu').slideToggle();
+    $('#map_filter_menu').slideToggle();
   },
+
   filterCategories: function(e){
     e.preventDefault()
 
     var cat_title = $(e.target).data('title')
     $('[title=' + cat_title + ']').toggle()
     $(e.target).toggleClass('filtered_off')
-    $(e.target).toggleClass('check_mark')
-
   }
 };
